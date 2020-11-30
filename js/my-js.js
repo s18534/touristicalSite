eebb4e1482c53e6d2b34aff9e02b95653b239968
s18534 <61833984+s18534@users.noreply.github.com>
2020-11-30 18:25:26 +0100

    adminPage

--- js/my-js.js
+++ js/my-js.js
@@ -2,13 +2,16 @@ class User{
     constructor(email, password){
         this.email = email;
         this.password = password;
+        this.username = 'User1';
+        this.isUserLoged = false;
+        this.isAdmin = true;
     }
 }
 
-let user = new User('exampleMail@gmail.com', 'easypassword');
+var user = new User('exampleMail@gmail.com', '123');
 
 //Login procedure
-$('#signInButton').on('click', function(user) {
+$('#signInButton').on('click', function() {
     let email = $("#inputEmail").val();
     let password = $('#inputPassword').val();
     if(email === '' || password === '')
@@ -17,8 +20,8 @@ $('#signInButton').on('click', function(user) {
         $('#userSection').html('');
     }
 
-    //login not working check it
-    //if(email == user.email && password == user.password)
+    //login
+    if(email === user.email && password === user.password) {
         $('#userSection').html('\
         <div class="nav-item dropdown mr-2">\
         <a class="nav-link dropdown-toggle text-muted" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
@@ -28,15 +31,37 @@ $('#signInButton').on('click', function(user) {
           <a class="dropdown-item" href="#">Favourite Places</a>\
           <a class="dropdown-item" href="#">Places to visit</a>\
           <div class="dropdown-divider"></div>\
-          <a class="dropdown-item" id="logout" href="#">Logout</a>\
+          <a class="dropdown-item"  id="logout" href="#">Logout</a>\
         </div>\
       </div>\
       ');
+      user.isUserLoged = true;
+    }
+
+    if(user.isAdmin){
+        window.open("admin.html");
+    }
+
 });
 
-//logout user from website TO ADD
-$('#logout').on('click', function(){
-    
+
+//logout user from website
+$(document).on('click','#logout', function(){
+    console.log('logout');
+    $('#userSection').html('\
+    <div class="form-group mb-2">\
+    <label for="inputEmail" class="sr-only">Email address</label>\
+    <input type="email" class="form-control" id="inputEmail" placeholder="Email address">\
+  </div>\
+  <div class="form-group mx-sm-3 mb-2">\
+    <label for="inputPassword" class="sr-only">Password</label>\
+    <input type="password" class="form-control" id="inputPassword" placeholder="Password">\
+  </div>\
+  <button type="submit" id="signInButton" class="btn btn-outline-primary bg-light mb-2 ">Sign in</button>\
+  <button type="submit" id="registerButton" class="btn  btn-outline-primary bg-light mb-2 ml-2 ">\
+    <a href="register.html" target="_blank">Register</a>\
+  </button>');
+  user.isUserLoged = false;
 });
 
 //==============REGISTRATION=======================
@@ -54,6 +79,40 @@ $('#submitRegistration').on('click', function(){
         alert('Values are not the same');
     }
 
+    //sending to base
+
 });
 
+//=================Admin Page======================
 
+	
+	// Edit row on edit button click to ADD
+$(document).on("click", "#edit", function(){		
+    
+});
+
+	// Delete row on delete button click
+$(document).on("click", "#delete", function(){
+    $(this).parents("tr").remove();
+	$("#addNew").removeAttr("disabled");
+});
+
+function userAdd(email, password, username) {
+    $("#productTable").append("<tr>" + 
+        "<td>" + email + "</td>" +
+        "<td>" + password + "</td>" +
+        "<td>" + username + "</td>" +
+        '<td>\
+        <button type="submit" id="edit" class="btn btn-outline-primary bg-light mb-2 ">Edit</button>\
+        <button type="submit" id="delete" class="btn  btn-outline-primary bg-light mb-2 ml-2 ">Delete</button>\
+        </td>' +
+        "</tr>");
+}
+
+ //adding user
+$('#saveUser').on('click', function() {
+    let email = $('#adminEmail').val();
+    let username = $('#adminUsername').val();
+    let password = $('#adminPassword').val();
+    userAdd(email, username, password);
+});
